7028acea3883de6aeadd8c55432abd0bf172af81
AlexCanchari <alexanderfish89@gmail.com>
2022-12-02 02:47:11 -0500

    a

--- src/model/index.js
+++ src/model/index.js
@@ -1,1 +1,8 @@
 export { default as Usuario } from './usuario.js'
+export { default as orden } from './orden.js'
+export { default as orden_producto } from './orden_producto.js'
+export { default as producto } from './producto.js'
+export { default as categoria } from './categoria.js'
+export { default as pre_armado_producto } from './pre_armado_producto.js'
+export { default as pre_armado } from './pre_armado.js'
+
