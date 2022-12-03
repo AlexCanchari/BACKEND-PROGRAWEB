cfac725b352bda1594a341d5c6d0066615e1a244
Giancarlo19Lopez <giancarlo19042002@hotmail.com>
2022-12-03 04:52:05 -0500

    subiendo back producto

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import bodyParser from 'body-parser'
 
 import usuarioRoutes from './src/routes/usuario.js'
 import prearmadoRoutes from './src/routes/prearmado.js'
-
+import productoRoutes from './src/routes/producto.js'
++
 const PORT = process.env.PORT || 4000
 
 var app = express();
@@ -15,6 +16,7 @@ app.use(bodyParser.json());
 
 app.use("/usuario", usuarioRoutes);
 app.use("/prearmado", prearmadoRoutes);
+app.use("/producto", productoRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto: ${PORT}`)
@@ -30,6 +32,20 @@ app.get("/prearmado", async (req, res) => {
     let piezas = []
 
     const pc = await Pre_Armado.findOne({where : {pre_armado_id : id_build}})
+    const pc_componentes = await Pre_Armado_Producto.findAll({where : {pre_armado_id : pc.pre_armado_id}})
+
+    for (let index = 0; index < pc_componentes.length; index++) {
+        const pieza = await Producto.findOne({where : {producto_id : pc_componentes[index].producto_id}})
+        pieza.push(pieza)
+    }
+
+    res.send(piezas)
+})
+app.get("/producto", async (req, res) => {
+    const id_build = req.query.id
+    let piezas = []
+
+    const pc = await Producto.findOne({where : {producto_id : id_build}})
     const pc_componentes = await Pre_Armado_Producto.findAll({where : {pre_armado_id : pc.pre_armado_id}})
 
     for (let index = 0; index < pc_componentes.length; index++) {
